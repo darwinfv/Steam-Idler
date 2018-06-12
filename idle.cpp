5d236c7abcc81f86e26056e2d77f03ce63361dcf
darwinfv <dvaz@purdue.edu>
2018-06-11 21:13:45 -0400

    Idle success, check mem

--- idle.cpp
+++ idle.cpp
@@ -3,30 +3,60 @@
 #include <string>
 #include <fstream>
 #include <dirent.h>
+#include <windows.h>
 using namespace std;
 
 const string path = "C:\\Program Files (x86)\\Steam\\";
 
-int main() {
+//void startup (LPCTSTR lpApplicationName) {
+//	STARTUPINFO si;
+//	PROCESS_INFORMATION pi;
+//	
+//	ZeroMemory( &si, sizeof(si) );
+//	si.cb = sizeof(si);
+//	ZeroMemory( &pi, sizeof(pi) );
+//	
+//	CreateProcess( lpApplicationName, "idle", NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi );
+//	
+//	CloseHandle( pi.hProcess );
+//    CloseHandle( pi.hThread );
+//}
+
+int main(int argc, char ** argv) {
 	
 	int app;
-	const string abs = "steamapps\\common\\Team Fortress 2\\";
-	const string folder = path + abs;
+	string abs = "steamapps\\common\\Team Fortress 2\\";
+	abs = path + abs;
 	
 	DIR *dir;
 	struct dirent *ent;
-	if ((dir = opendir (folder.c_str())) != NULL) {
+	if ((dir = opendir (abs.c_str())) != NULL) {
+		closedir(dir);
 		cout<<"Enter App ID: ";
 	}
 	else {
 	  cout<<"ERROR: Incorrect path specified";
 	  getch();
-	  exit();
+	  exit(1);
 	}
 	
 	cin>>app;
-	
-	
+	string steam = abs + "steam_appid.txt";
+
+	ofstream f;
+	f.open(steam.c_str(), ios::trunc);
+	if (!f) {
+        cout<<"ERROR: Unable to open file";
+        getch();
+        exit(1);
+    }
+    
+    f<<app;
+    f<<" ";
+    f.close();
+    
+    string exe = abs + "hl2.exe";
+    ShellExecute(NULL, "open", exe.c_str(), NULL, NULL, SW_SHOWDEFAULT);
 	
 	getch();
 	
