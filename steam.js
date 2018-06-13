dca31a409700686b05ce469e71370320667abd4b
darwinfv <dvaz@purdue.edu>
2018-06-13 18:35:35 -0400

    Need authentication

--- steam.js
+++ steam.js
@@ -2,23 +2,22 @@
 	const axios = require('axios');
 	const cheerio = require('cheerio');
 
-	axios.get("https://steamcommunity.com/profiles/76561198052935979/badges/")
+	axios.get("https://steamcommunity.com/profiles/76561198052935979/badges/?sort=p")
 		.then((response) => {
 			if(response.status === 200) {
 				const html = response.data;
 				const $ = cheerio.load(html);
-				console.log("Perforemd request");
-				$('.badge_row_overlay').each(function(i, elem) {
-					// conosle.log("In loop");
+				$('.badge_row').each(function(i, elem) {
 					// var drops = $(this).find().children('.progress_info_bold').text();
 					// var drops = $(this).find('.card_drop_info_header').text();
-					let drops = $(this).attr('href');
-					console.log("!" + drops + "!");
-					// return;
+					let appid = $(this).find('.badge_row_overlay').attr('href');
+					if(appid.includes("gamecards")) {
+						console.log(appid);
+					}
 				});
 			}
-			// else {
-				// console.log("Response code: " + response.status);
-			// }
+			else {
+				console.log("Response code: " + response.status);
+			}
 		}, (error) => console.log(err) );
 
