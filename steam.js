74b2f02fab44b367f912f695c8de79d018cc5aed
darwinfv <dvaz@purdue.edu>
2018-06-13 14:34:39 -0400

    Fixed tag detection

--- steam.js
+++ steam.js
@@ -1,21 +1,24 @@
 
 	const axios = require('axios');
+	const cheerio = require('cheerio');
 
-	const key = "DE783384768F713588C69088F368AD58";
-	const http = "http://api.steampowered.com/";
-	const id = "76561198052935979/";
-
-	var link = http + "IPlayerService/GetOwnedGames/v0001/?key=" + key + "&steamid=" + id;
-	console.log(link);
-	
 	axios.get("https://steamcommunity.com/profiles/76561198052935979/badges/")
 		.then((response) => {
 			if(response.status === 200) {
-				console.log(response.data);
-				// var info = response.data;
-				// console.log(info.response.games);
+				const html = response.data;
+				const $ = cheerio.load(html);
+				console.log("Perforemd request");
+				$('.badge_row_overlay').each(function(i, elem) {
+					// conosle.log("In loop");
+					// var drops = $(this).find().children('.progress_info_bold').text();
+					// var drops = $(this).find('.card_drop_info_header').text();
+					let drops = $(this).attr('href');
+					console.log("!" + drops + "!");
+					// return;
+				});
 			}
-			else
-				console.log("Response code: " + response.status);
-
+			// else {
+				// console.log("Response code: " + response.status);
+			// }
 		}, (error) => console.log(err) );
+
