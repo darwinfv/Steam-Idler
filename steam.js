ef37ab797cc39ca2d43772b215ddf649d694ebe2
darwinfv <dvaz@purdue.edu>
2018-06-13 21:26:50 -0400

    Access to span tags needed

--- steam.js
+++ steam.js
@@ -11,12 +11,21 @@
 				$('.badge_row').each(function(i, elem) {
 					// var drops = $(this).find().children('.progress_info_bold').text();
 					// var drops = $(this).find('.card_drop_info_header').text();
-					let appid = $(this).find('.badge_row_overlay').attr('href');
-					if(appid.includes("gamecards")) {
-						appid = appid.substring(appid.lastIndexOf("/", appid.length - 2) + 1, appid.length - 1);
-						let drops = $(this).find('.progress_info_bold').text();
-						console.log(drops);
-					}
+					// let appid = $(this).find('.progress_info_bold').text();
+					// let appid = $(this).find('.badge_row_overlay').attr('href');
+					// if(appid.includes("gamecards")) {
+						// appid = appid.substring(appid.lastIndexOf("/", appid.length - 2) + 1, appid.length - 1);
+						// let drops = $(this).find('.card_drop_info_body').text();
+						let appid = $(this).children('.badge_row_inner')
+											.children('.badge_title_row')
+											.children('.badge_title_stats')
+											.children('.badge_title_stats_content')
+											.children('.badge_title_stats_drops')
+											.find('.how_to_get_card_drops')
+											.attr('href');
+
+						console.log(appid);
+					// }
 				});
 			}
 			else {
