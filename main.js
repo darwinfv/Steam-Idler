7fd95902dd7d95bc719227033fd6688579eb3dfb
darwinfv <dvaz@purdue.edu>
2018-06-15 00:15:28 -0400

    Added electron dependencies

--- main.js
+++ main.js
@@ -7,18 +7,18 @@
 	// const child = proc.spawn(electron);
 
 	// In the main process.
-const {BrowserWindow} = require('electron')
+	const {BrowserWindow} = require('electron')
 
-// Or use `remote` from the renderer process.
-// const {BrowserWindow} = require('electron').remote
+	// Or use `remote` from the renderer process.
+	// const {BrowserWindow} = require('electron').remote
 
-let win = new BrowserWindow({width: 800, height: 600})
-win.on('closed', () => {
-  win = null
-})
+	let win = new BrowserWindow({width: 800, height: 600})
+	win.on('closed', () => {
+	  win = null
+	})
 
-// Load a remote URL
-win.loadURL('https://github.com')
+	// Load a remote URL
+	win.loadURL('https://github.com')
 
-// Or load a local HTML file
-win.loadURL(`file://${__dirname}/app/index.html`)
+	// Or load a local HTML file
+	win.loadURL(`file://${__dirname}/app/index.html`)
