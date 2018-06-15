d099de95104c1fcacfb04e69b58ecf4d0f589001
darwinfv <dvaz@purdue.edu>
2018-06-15 00:01:23 -0400

    Incorporate Electron into app

---
+++ main.js
@@ -0,0 +1,24 @@
+	
+	const electron = require('electron');
+	const proc = require('child_process');
+
+	console.log(electron);
+
+	// const child = proc.spawn(electron);
+
+	// In the main process.
+const {BrowserWindow} = require('electron')
+
+// Or use `remote` from the renderer process.
+// const {BrowserWindow} = require('electron').remote
+
+let win = new BrowserWindow({width: 800, height: 600})
+win.on('closed', () => {
+  win = null
+})
+
+// Load a remote URL
+win.loadURL('https://github.com')
+
+// Or load a local HTML file
+win.loadURL(`file://${__dirname}/app/index.html`)
