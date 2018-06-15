40c24f695391f27043f43f4fa3791e425304f331
darwinfv <dvaz@purdue.edu>
2018-06-15 13:00:33 -0400

    Success init electron, build pages

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
-	
-	const electron = require('electron');
-	const proc = require('child_process');
 
-	console.log(electron);
+	const {app, BrowserWindow} = require('electron');
 
-	// const child = proc.spawn(electron);
+	function createWindow () {
+		win = new BrowserWindow({width: 800, height: 600});
+		win.loadFile('index.html');
+	}
 
-	// In the main process.
-	const {BrowserWindow} = require('electron')
-
-	// Or use `remote` from the renderer process.
-	// const {BrowserWindow} = require('electron').remote
-
-	let win = new BrowserWindow({width: 800, height: 600})
-	win.on('closed', () => {
-	  win = null
-	})
-
-	// Load a remote URL
-	win.loadURL('https://github.com')
-
-	// Or load a local HTML file
-	win.loadURL(`file://${__dirname}/app/index.html`)
+	app.on('ready', createWindow);
