5cb5b31da9cdb3348c154e6c70fb25a845a7612b
darwinfv <dvaz@purdue.edu>
2018-06-18 20:08:32 -0400

    Added js file for button functions, etc

---
+++ functions.js
@@ -0,0 +1,11 @@
+
+    // Enable tooltips
+    $(document).ready(function(){
+        $('[data-toggle="tooltip"]').tooltip(); 
+    });
+
+    function idleSingle() {
+        var path = document.getElementById("path").value;
+        var exe = document.getElementById("exe").value;
+        var appid = document.getElementById("appid").value;
+    }
