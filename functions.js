9a5787c7be88c0c1c8022fa8f320a72de48a710c
darwinfv <dvaz@purdue.edu>
2018-06-19 20:43:59 -0400

    Improved gui, working on bash script

--- functions.js
+++ functions.js
@@ -15,7 +15,7 @@
 
         var exec = require('child_process').exec, child;
 
-        child = exec('.\\execute.sh ' + appid + ' ' + path + ' ' + exe,
+        child = exec('.\\execute.sh ' + appid + ' \"' + path + '\" \"' + exe + '\"',
             function (error, stdout, stderr) {
                 console.log('stdout: ' + stdout);
                 console.log('stderr: ' + stderr);
@@ -24,6 +24,27 @@
                 }
             });
 
-        
+
+    }
+
+    function idleAll() {
+        var path = document.getElementById("path").value;
+        var exe = document.getElementById("exe").value;
+        var appid = document.getElementById("appid").value;
+        if(appid == "") {
+            ;
+            return;
+        }
+
+        var exec = require('child_process').exec, child;
+
+        child = exec('.\\execute.sh ' + appid + ' \"' + path + '\" \"' + exe + '\"',
+            function (error, stdout, stderr) {
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+                if (error !== null) {
+                     console.log('exec error: ' + error);
+                }
+            });
     }
 
