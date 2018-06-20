31141ddb7b374cbb6683f9ce91df162e823bd5e3
darwinfv <dvaz@purdue.edu>
2018-06-20 17:10:06 -0400

    Add file extraction and counting to js

--- functions.js
+++ functions.js
@@ -40,6 +40,9 @@
 
         var exec = require('child_process').exec, child;
         const fs = require('fs');
+        const cheerio = require('cheerio');
+
+        let games = [];
 
         child = exec('.\\checkFile.exe',
             function (error, stdout, stderr) {
@@ -54,6 +57,28 @@
                         $('#alert').html("File missing: 'badges.html'");
                         $('#alert').show();
                         return;
+                    }
+                    if(data == 0) {
+                        fs.readFile('.\\badges.html', 'utf8', function (err, data) {
+                            if(err) throw err;
+                            const $ = cheerio.load(data);
+                            var n = 0;
+                            $('.badge_title_stats_drops').each(function(i, elem) {
+                                let drops = $(this).find('.progress_info_bold').text();
+                                drops = drops.substring(0, 1);
+                                if(drops != "" && drops != undefined && drops != null && drops != "N") {
+                                    let info = $(this).find('a').attr('href');
+                                    let name = info.substring(30, info.indexOf("\"", 31));
+                                    let appid = info.substring(info.lastIndexOf("_", info.length - 9) + 1, info.length - 8);
+                                    games[n] = [];
+                                    games[n][0] = drops;
+                                    games[n][1] = name;
+                                    games[n][2] = appid;
+                                    n++;
+                                }
+                            });
+                            console.log(games);
+                        });
                     }
                 });
             });
