d75fe2fbda3ea6f3ebdc27ff01641eb34dea3d64
darwinfv <dvaz@purdue.edu>
2018-06-22 23:02:12 -0400

    Bash test working, incorporate js with bat

--- cmdtest.sh
+++ cmdtest.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-path="potato"
+path=".\\node_modules\\"
 #"C:\\Program Files (x86)\\Steam\\steamapp\\common\\Team Fortress 2\\";
 exe=""
 #"hl2.exe";
 appid=65930
 
+.\\idle.exe $appid
 
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo $?
 
-if(path == "" || path == null || path == undefined) {
-    child = exec('.\\idle.exe ' + appid,
-        function (error, stdout, stderr) {
-            if (error !== null) {
-                 console.log(error);
-            }
-            fs.readFile('.\\info.txt', 'utf8', function (err, data) {
-	            if(err) throw err;
-				console.log(data);
-	        });
-        });
-}
+cat info.txt
